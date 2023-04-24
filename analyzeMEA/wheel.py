fdb6aadc40fd6a944f61bd7b2ecff76f06bd01ee
Alan Emanuel <alan.emanuel@emory.edu>
2023-04-24 17:09:58 -0400

    added permutation test for ID of OS units

--- analyzeMEA/wheel.py
+++ analyzeMEA/wheel.py
@@ -78,11 +78,135 @@ def classify_gratings_manually(footfallImages,savename = 'grating_classification
         np.save(savename,np.int32(grating_classification))
 
 
+def permuteOS(trialGratings, units, numShuffles=10000, plot=True):
+    
 
+    ## set random seed and create generator
+    
+    rng = np.random.default_rng(20230424)
+    numUnits = len(units)
+    oris = []
+    responses = []
+    weights = []
+
+    for ori in trialGratings:
+        oris.append(np.ones(len(trialGratings[ori]))*ori)
+        responses.append(trialGratings[ori])
+        weights.append(np.ones(len(trialGratings[ori]))*(1/len(trialGratings[ori])))
+
+    oris = np.concatenate(oris)
+    responses = np.concatenate(responses)
+    weights = np.concatenate(weights)
+    vector = np.zeros([numUnits,len(responses),2]) ## Units x Footfalls x XY
+    for i, response in enumerate(responses):
+        if oris[i] == 1:
+            vector[:,i,1] = response ## positive Y
+        elif oris[i] == 2:
+            vector[:,i,0] = response ## positive X
+        elif oris[i] == 3:
+            vector[:,i,1] = -response ## negative Y
+        elif oris[i] == 4:
+            vector[:,i,0] = -response ## negative X
+    
+    shuffleVectors = np.zeros([len(responses),numUnits,numShuffles,2])
+    shuffleOris = np.zeros([len(responses),numShuffles])
+    shuffleWeights = np.zeros([len(responses),numShuffles])
+    for shuffle in range(numShuffles):
+        permIndex = rng.permutation(np.arange(len(responses)))
+        shuffleOris[:,shuffle] = oris[permIndex]
+        shuffleWeights[:,shuffle] = weights[permIndex]
+
+        for i, response in enumerate(responses):
+            if shuffleOris[i,shuffle] == 1:
+                shuffleVectors[i,:,shuffle,1] = response ## positive Y
+            elif shuffleOris[i,shuffle] == 2:
+                shuffleVectors[i,:,shuffle,0] = response ## positive X
+            elif shuffleOris[i,shuffle] == 3:
+                shuffleVectors[i,:,shuffle,1] = -response ## negative Y
+            elif shuffleOris[i,shuffle] == 4:
+                shuffleVectors[i,:,shuffle,0] = -response ## negative X
+
+    meanVector = np.average(vector,axis=1,weights=weights)
+    meanShuffleVectors = np.zeros([numUnits,numShuffles,2])
+
+    for unit in range(numUnits):
+        meanShuffleVectors[unit, :, 0] = np.average(shuffleVectors[:,unit,:,0],axis=0,weights=shuffleWeights)
+        meanShuffleVectors[unit, :, 1] = np.average(shuffleVectors[:,unit,:,1],axis=0,weights=shuffleWeights)
+
+
+    distance = (meanVector[:,0]**2 + meanVector[:,1]**2)**0.5
+    shuffleDistances = (meanShuffleVectors[:,:,0]**2 + meanShuffleVectors[:,:,1]**2)**0.5
+
+    pvalues = np.ones(len(units)) - np.sum(np.matlib.repmat(distance,numShuffles,1).T > shuffleDistances,axis=1)/numShuffles
+    
+    import statsmodels.stats.multitest
+    OSunits = units[np.where(statsmodels.stats.multitest.multipletests(pvalues,method='hs')[0])[0]]
+    OSunits_noCorrection = units[pvalues < 0.05]
+
+    print('Without multiple comparison correction:')
+    print('{} of {} units are OS'.format(len(OSunits_noCorrection),len(units)))
+    print('Units {}'.format(OSunits_noCorrection))
+
+    print('\nWith HS correction:')
+    print('{} of {} units are OS'.format(len(OSunits),len(units)))
+    print('Units {}'.format(OSunits))
+
+    if plot:
+        theta = np.arctan2(meanVector[:,1],meanVector[:,0])
+        thetaShuffled = np.arctan2(meanShuffleVectors[:,:,1],meanShuffleVectors[:,:,0])
+        
+        for unit in range(len(units)):
+            plt.figure()
+            plt.polar([0,theta[unit]],[0,distance[unit]],color='r',label='actual')
+            plt.polar(thetaShuffled[unit,:],shuffleDistances[unit,:],'.',color='gray',label='shuffle',alpha=0.2)
+            if units[unit] in OSunits:
+                plt.title('Unit {}, p = {}'.format(units[unit],pvalues[unit]),color='r')
+            else:
+                plt.title('Unit {}, p = {}'.format(units[unit],pvalues[unit]),color='k')
+            plt.show()
+            plt.close()
+
+    
+    return pvalues, OSunits, OSunits_noCorrection, meanVector, meanShuffleVectors
+
+
+        
 
 
 
 ### helper functions
+
+# def TwoSampleT2Test(X, Y):
+#     from scipy.stats import f
+#     nx , p = X.shape
+#     ny, _ = Y.shape
+#     delta = np.mean(X, axis=0) - np.mean(Y, axis=0)
+#     Sx = np.cov(X, rowvar=False)
+#     Sy = np.cov(Y, rowvar=False)
+#     S_pooled = ((nx-1)*Sx + (ny-1)*Sy)/(nx+ny-2)
+#     t_squared = (nx*ny)/(nx+ny) * np.matmul(np.matmul(delta.transpose(), np.linalg.inv(S_pooled)), delta)
+#     statistic = t_squared * (nx+ny-p-1)/(p*(nx+ny-2))
+#     F = f(p, nx+ny-p-1)
+#     p_value = 1 - F.cdf(statistic)
+#     print(f"test statistic: {statistic}\nDegrees of freedom: {p} and {nx+ny-p-1}\np-value: {p_value}")
+#     return statistic, p_value
+
+def OneSampleT2Test(X, nullHyp = None, verbose=False):
+    from scipy.stats import f
+    nx , k = X.shape
+    if nullHyp is None:
+        nullHyp = np.zeros([1,k]) ## null hypothesis is zero
+    delta = np.mean(X, axis=0) - nullHyp
+    Sx = np.cov(X, rowvar=False)
+    t_squared = nx * np.matmul(np.matmul(delta, np.linalg.inv(Sx)), delta.T)
+    statistic = t_squared * (nx-k)/(k*(nx-1))
+    F = f(k, nx-k)
+    p_value = float(1 - F.cdf(statistic))
+    if verbose:
+        print(f"test statistic: {statistic}\nDegrees of freedom: {k} and {nx-k}\np-value: {p_value}")
+    return statistic, p_value
+
+
 def rotate_image(image, angle):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
